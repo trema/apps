084967a3654699914e23de90069d81b915fa18ab
SUGYO <sugyo@pb.jp.nec.com>
2013-07-29 16:52:14 +0900

    fix flow_dumper.c to support trema-edge
    Build flow_dumper for trema-edge
      make TREMA_CONFIG=/somewhere/trema-edge/trema-config
    
    How to use?
      cd /somewhere/trema-edge
      ./trema run src/examples/learning_switch/learning-switch.rb -c src/examples/learning_switch/sample.conf -d
      ./trema run /somewhere/apps/flow_dumper/flow_dumper

--- flow_dumper/flow_dumper.c
+++ flow_dumper/flow_dumper.c
@@ -3,7 +3,7 @@
  *
  * Author: Yasunobu Chiba
  *
- * Copyright (C) 2011 NEC Corporation
+ * Copyright (C) 2011-2013 NEC Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
@@ -33,10 +33,15 @@ static void
 handle_list_switches_reply( const list_element *switches, void *user_data ) {
   UNUSED( user_data );
 
+#ifdef TREMA_EDGE
+  buffer *stats_request = create_flow_multipart_request( get_transaction_id(), 0,
+                                                         OFPTT_ALL, OFPP_ANY, OFPG_ANY, 0, 0, NULL );
+#else
   struct ofp_match match;
   memset( &match, 0, sizeof( struct ofp_match ) );
   match.wildcards = OFPFW_ALL;
   buffer *stats_request = create_flow_stats_request( get_transaction_id(), 0, match, 0xff, OFPP_NONE );
+#endif
 
   n_switches = 0;
   const list_element *element;
@@ -52,6 +57,34 @@ handle_list_switches_reply( const list_element *switches, void *user_data ) {
 
 static void
 dump_flow_stats( uint64_t datapath_id, struct ofp_flow_stats *stats ) {
+#ifdef TREMA_EDGE
+  uint16_t match_len = ( uint16_t ) ( stats->match.length + PADLEN_TO_64( stats->match.length ) );
+
+  char match_str[ 2048 ];
+  memset( match_str, '\0', sizeof( match_str ) );
+  { // XXX
+    struct ofp_match *tmp_match = xcalloc( 1, match_len );
+    hton_match( tmp_match, &stats->match );
+    oxm_matches *tmp_matches = parse_ofp_match( tmp_match );
+    match_to_string( tmp_matches, match_str, sizeof( match_str ) );
+    xfree( tmp_match );
+    delete_oxm_matches( tmp_matches );
+  }
+  char inst_str[ 4096 ];
+  memset( inst_str, '\0', sizeof( inst_str ) );
+  uint16_t inst_len = ( uint16_t ) ( stats->length - ( offsetof( struct ofp_flow_stats, match ) + match_len ) );
+  if ( inst_len > 0 ) {
+    struct ofp_instruction *inst = ( struct ofp_instruction * ) ( ( char * ) stats + offsetof( struct ofp_flow_stats, match ) + match_len );
+    instructions_to_string( inst, inst_len, inst_str, sizeof( inst_str ) );
+  }
+
+  info( "[%#016" PRIx64 "] table_id = %u, priority = %u, cookie = %#" PRIx64 ", idle_timeout = %u, "
+        " hard_timeout = %u, duration = %u.%09u, packet_count = %" PRIu64 ", byte_count = %" PRIu64
+        ", match = [%s], actions = [%s]",
+        datapath_id, stats->table_id, stats->priority, stats->cookie, stats->idle_timeout,
+        stats->hard_timeout, stats->duration_sec, stats->duration_nsec, stats->packet_count, stats->byte_count,
+        match_str, inst_str );
+#else
   char match_str[ 512 ];
   memset( match_str, '\0', sizeof( match_str ) );
   match_to_string( &stats->match, match_str, sizeof( match_str ) );
@@ -69,6 +102,27 @@ dump_flow_stats( uint64_t datapath_id, struct ofp_flow_stats *stats ) {
         datapath_id, stats->table_id, stats->priority, stats->cookie, stats->idle_timeout,
         stats->hard_timeout, stats->duration_sec, stats->duration_nsec, stats->packet_count, stats->byte_count,
         match_str, actions_str );
+#endif
+}
+
+
+static bool
+type_is_flow_stats( uint16_t type ) {
+#ifdef TREMA_EDGE
+  return type == OFPMP_FLOW;
+#else
+  return type == OFPST_FLOW;
+#endif
+}
+
+
+static bool
+more_requests( uint16_t flags ) {
+#ifdef TREMA_EDGE
+  return ( flags & OFPMPF_REPLY_MORE ) == OFPMPF_REPLY_MORE;
+#else
+  return ( flags & OFPSF_REPLY_MORE ) == OFPSF_REPLY_MORE;
+#endif
 }
 
 
@@ -79,7 +133,7 @@ handle_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
   UNUSED( transaction_id );
   UNUSED( user_data );
 
-  if ( type != OFPST_FLOW ) {
+  if ( !type_is_flow_stats( type ) ) {
     return;
   }
 
@@ -92,7 +146,7 @@ handle_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
     }
   }
 
-  if ( ( flags & OFPSF_REPLY_MORE ) == 0 ) {
+  if ( !more_requests( flags ) ) {
     if ( --n_switches <= 0 ) {
       stop_trema();
     }
@@ -117,7 +171,11 @@ main( int argc, char *argv[] ) {
 
   // Set event handlers
   set_list_switches_reply_handler( handle_list_switches_reply );
+#ifdef TREMA_EDGE
+  set_multipart_reply_handler( handle_stats_reply, NULL );
+#else
   set_stats_reply_handler( handle_stats_reply, NULL );
+#endif
 
   // Ask switch manager to obtain the list of switches
   send_list_switches_request( NULL );
