bd6be27fb9849bdab1a1c4c12d462715f7d348b3
SUGYO <sugyo@pb.jp.nec.com>
2013-07-30 16:18:10 +0900

    fix show_description.c to support trema-edge
    Build:
      cd /somewhere/apps/show_description
      make TREMA_CONFIG=/somewhere/trema-edge/trema-config
    Example:
      cd /somewhere/trema-edge
      ./trema run src/examples/learning_switch/learning-switch.rb -c src/examples/learning_switch/sample.conf -d
      ./trema run /somewhere/apps/show_description/show_description

--- show_description/show_description.c
+++ show_description/show_description.c
@@ -1,7 +1,7 @@
 /*
  * Author: SUGYO Kazushi
  *
- * Copyright (C) 2012 NEC Corporation
+ * Copyright (C) 2012-2013 NEC Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
@@ -28,7 +28,12 @@
 
 typedef struct {
   uint64_t datapath_id;
+#ifdef TREMA_EDGE
+  struct ofp_desc desc_stats;
+  list_element *port_desc;
+#else
   struct ofp_desc_stats desc_stats;
+#endif
 } desc_entry;
 
 
@@ -42,30 +47,63 @@ static void
 handle_list_switches_reply( const list_element *switches, void *user_data ) {
   show_desc *show_desc = user_data;
 
+#ifdef TREMA_EDGE
+  buffer *desc_stats_request = create_desc_multipart_request( get_transaction_id(), 0 );
+#else
+  buffer *desc_stats_request = create_desc_stats_request( get_transaction_id(), 0 );
+#endif
+
   const list_element *element;
   for ( element = switches; element != NULL; element = element->next ) {
     uint64_t datapath_id = * ( uint64_t *) element->data;
-    buffer *desc_stats_request = create_desc_stats_request( get_transaction_id(), 0 );
     send_openflow_message( datapath_id, desc_stats_request );
-    free_buffer( desc_stats_request );
     show_desc->count++;
   }
+
+  free_buffer( desc_stats_request );
 }
 
 
+static bool
+type_is_desc_stats( uint16_t type ) {
+#ifdef TREMA_EDGE
+  return type == OFPMP_DESC;
+#else
+  return type == OFPST_DESC;
+#endif
+}
+
+#ifdef TREMA_EDGE
+static bool
+type_is_port_desc( uint16_t type ) {
+  return type == OFPMP_PORT_DESC;
+}
+
+
+static bool
+more_requests( uint16_t flags ) {
+  return ( flags & OFPMPF_REPLY_MORE ) == OFPMPF_REPLY_MORE;
+}
+#endif
+
+
+#ifdef TREMA_EDGE
+#define SIZE_OF_OFP_DESC sizeof( struct ofp_desc )
+#else
+#define SIZE_OF_OFP_DESC sizeof( struct ofp_desc_stats )
+#endif
+
+
 static void
-handle_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
-                    uint16_t type, uint16_t flags, const buffer *data,
-                    void *user_data ) {
+handle_desc_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
+                        uint16_t type, uint16_t flags, const buffer *data,
+                       void *user_data ) {
   UNUSED( transaction_id );
+  UNUSED( type );
   UNUSED( flags );
   show_desc *show_desc = user_data;
 
-  if ( type != OFPST_DESC ) {
-    return;
-  }
-
-  if ( data->length < sizeof( struct ofp_desc_stats ) ) {
+  if ( data->length < SIZE_OF_OFP_DESC ) {
     error( "invalid data" );
 
     stop_trema();
@@ -73,15 +111,118 @@ handle_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
   }
   desc_entry *entry = xmalloc( sizeof( desc_entry ) );
   entry->datapath_id = datapath_id;
-  memcpy( &entry->desc_stats, data->data, sizeof( struct ofp_desc_stats ) );
+#ifdef TREMA_EDGE
+  create_list( &entry->port_desc );
+#endif
+  memcpy( &entry->desc_stats, data->data, SIZE_OF_OFP_DESC );
   insert_hash_entry( show_desc->db, &entry->datapath_id, entry );
 
+#ifdef TREMA_EDGE
+  buffer *port_desc_request = create_port_desc_multipart_request( get_transaction_id(), 0 );
+  send_openflow_message( datapath_id, port_desc_request );
+  free_buffer( port_desc_request );
+#else
   buffer *features_request = create_features_request( get_transaction_id() );
   send_openflow_message( datapath_id, features_request );
   free_buffer( features_request );
+#endif
 }
 
 
+#ifdef TREMA_EDGE
+static void
+handle_port_desc_reply( uint64_t datapath_id, uint32_t transaction_id,
+                        uint16_t type, uint16_t flags, const buffer *data,
+                       void *user_data ) {
+  UNUSED( transaction_id );
+  UNUSED( type );
+  UNUSED( flags );
+  show_desc *show_desc = user_data;
+
+  desc_entry *desc = lookup_hash_entry( show_desc->db, &datapath_id );
+  if ( desc == NULL ) {
+    return;
+  }
+  append_to_tail( &desc->port_desc, duplicate_buffer( data ) );
+
+  if ( more_requests( flags ) ) {
+    return;
+  }
+
+  struct ofp_desc *desc_stats = &desc->desc_stats;
+  info( "Manufacturer description: %s", desc_stats->mfr_desc );
+  info( "Hardware description: %s", desc_stats->hw_desc );
+  info( "Software description: %s", desc_stats->sw_desc );
+  info( "Serial number: %s", desc_stats->serial_num );
+  info( "Human readable description of datapath: %s", desc_stats->dp_desc );
+  info( "Datapath ID: %#" PRIx64, datapath_id );
+  for ( list_element *e = desc->port_desc; e != NULL; e = e->next ) {
+    buffer *data = e->data;
+    struct ofp_port *port = ( struct ofp_port  * ) data->data;
+    size_t length = data->length;
+    while ( length >= sizeof( struct ofp_port ) ) {
+      const char *fake_output = "";
+      if ( port->port_no == OFPP_LOCAL ) {
+	fake_output = ":Local";
+      }
+      else if ( port->port_no == 0 || port->port_no > OFPP_MAX ) {
+	fake_output = ":Invalid port";
+      }
+      const char *state = "(Port up)";
+      if ( ( port->config & OFPPC_PORT_DOWN ) == OFPPC_PORT_DOWN &&
+	   ( port->state & OFPPS_LINK_DOWN ) == OFPPS_LINK_DOWN ) {
+	state = "(Port down)";
+      }
+      else if ( ( port->config & OFPPC_PORT_DOWN ) == OFPPC_PORT_DOWN ) {
+	state = "(Port is administratively down)";
+      }
+      else if ( ( port->state & OFPPS_LINK_DOWN ) == OFPPS_LINK_DOWN ) {
+	state = "(No physical link present)";
+      }
+      info( "Port no: %u(0x%x%s)%s", port->port_no, port->port_no, fake_output, state );
+      info(
+	"  Hardware address: %02x:%02x:%02x:%02x:%02x:%02x",
+	port->hw_addr[ 0 ],
+	port->hw_addr[ 1 ],
+	port->hw_addr[ 2 ],
+	port->hw_addr[ 3 ],
+	port->hw_addr[ 4 ],
+	port->hw_addr[ 5 ]
+      );
+      info( "  Port name: %s", port->name );
+      length -= ( uint16_t ) sizeof( struct ofp_port );
+      port++;
+    }
+    free_buffer( e->data );
+  }
+  delete_list( desc->port_desc );
+  delete_hash_entry( show_desc->db, &datapath_id );
+  show_desc->count--;
+  xfree( desc );
+
+  if ( show_desc->count == 0 ) {
+    stop_trema();
+  }
+}
+#endif
+
+
+static void
+handle_stats_reply( uint64_t datapath_id, uint32_t transaction_id,
+                    uint16_t type, uint16_t flags, const buffer *data,
+                    void *user_data ) {
+  if ( type_is_desc_stats( type ) ) {
+    handle_desc_stats_reply( datapath_id, transaction_id, type, flags, data, user_data );
+  }
+#ifdef TREMA_EDGE
+  else if ( type_is_port_desc( type ) ) {
+    handle_port_desc_reply( datapath_id, transaction_id, type, flags, data, user_data );
+  }
+#endif
+}
+
+
+#ifndef TREMA_EDGE
 static void
 handle_features_reply( uint64_t datapath_id, uint32_t transaction_id, uint32_t n_buffers, uint8_t n_tables, uint32_t capabilities, uint32_t actions, const list_element *phy_ports, void *user_data) {
   UNUSED( transaction_id );
@@ -108,10 +249,10 @@ handle_features_reply( uint64_t datapath_id, uint32_t transaction_id, uint32_t n
     struct ofp_phy_port *phy_port = port->data;
     const char *fake_output = "";
     if ( phy_port->port_no == OFPP_LOCAL ) {
-	fake_output = ":Local";
+      fake_output = ":Local";
     }
     else if ( phy_port->port_no == 0 || phy_port->port_no > OFPP_MAX ) {
-	fake_output = ":Invalid port";
+      fake_output = ":Invalid port";
     }
     const char *state = "(Port up)";
     if ( ( phy_port->config & OFPPC_PORT_DOWN ) == OFPPC_PORT_DOWN &&
@@ -145,6 +286,7 @@ handle_features_reply( uint64_t datapath_id, uint32_t transaction_id, uint32_t n
     stop_trema();
   }
 }
+#endif
 
 
 static void
@@ -165,8 +307,12 @@ main( int argc, char *argv[] ) {
   show_desc.count = 0;
 
   set_list_switches_reply_handler( handle_list_switches_reply );
+#ifdef TREMA_EDGE
+  set_multipart_reply_handler( handle_stats_reply, &show_desc );
+#else
   set_stats_reply_handler( handle_stats_reply, &show_desc );
   set_features_reply_handler( handle_features_reply, &show_desc );
+#endif
   add_periodic_event_callback( 10, timed_out, NULL );
 
   send_list_switches_request( &show_desc );
